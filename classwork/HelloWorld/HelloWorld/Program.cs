8471e4ebeb0191d61de6dc3d12e29ab908e838e6
VALERIE BUSTILLOS <VALERIE.BUSTILLOS2@tccd.edu>
2022-08-24 21:36:19 -0500

    Hello World and Section1 Programs

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿//
+//  Valerie Bustillos
+//  ITSE 1430 2022 FL
+//  Classroom Work 08/24/22 _ Program1 (Use Lab # if for labs)
+//
+namespace HelloWorld
+{
+    /*
+     * A multiline comment
+     */
+    internal class Program
+    {
+        // A comment
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+
+            // Another Block of Code
+        }
+    }
+}
