0d1b5308af9f198c209a7a83ed29334b3173e16c
VALERIE BUSTILLOS <VALERIE.BUSTILLOS2@tccd.edu>
2022-08-29 21:05:57 -0500

    Start of Movie Library Project

---
+++ classwork/Section1/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,14 @@
+﻿//Movie definition
+string title = "";
+string description = "";
+int runLength = 0;          //in minutes
+int releaseYear = 1900;
+string rating = "";
+bool isClassic = false;
+
+Console.WriteLine("Enter a title: ");
+Console.WriteLine("Enter an optional description: ");
+Console.WriteLine("Enter a run length (in Minutes): ");
+Console.WriteLine("Enter the release year: ");
+Console.WriteLine("Enter the MPAA rating: ");
+Console.WriteLine("Is this a classic? ");
--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -11,3 +11,33 @@ value = Console.ReadLine();
 double payRate  = Double.Parse(value);
 
 Console.WriteLine("Your pay is " + (hours * payRate));
+
+int x;
+double distanceFromMoon = 0;
+//distanceFromMoon = 0;
+char letterGrade;
+bool isPassing;
+
+string name;
+string firstName = "Bob", lastName = "Smith";
+
+firstName = lastName = name;
+//firstName = "Sam";
+//lastName = "Sam";
+
+//string lastName;
+
+//int iHours;
+//float fRate;
+
+
+
+//Block Statement
+{
+    decimal payRate;
+}
+
+//distanceFromMoon = 10.5;
+isPassing = distanceFromMoon > 10;
+
+
